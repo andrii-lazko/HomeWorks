42e7f68bf32b95346a2bb3dac5387d1bb7527a28
andrii-lazko <andrii.lazko.email@gmail.com>
2025-06-18 11:59:25 +0300

    Added name field

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Andrii";
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, World!");
+
+Console.ReadLine
 
