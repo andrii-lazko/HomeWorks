02ce85bd5ecd0cd2f5b44f49db118e2dd8bcac93
andrii-lazko <andrii.lazko.email@gmail.com>
2025-06-18 12:12:45 +0300

    Revert "added last name"
    This reverts commit 642f3dddd094ebe7169e1a52bb5c1c7226efccb9.

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -1,11 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string name = "Andrii";
-string lastname = "Lazko";
 
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, World!");
 
-Console.ReadLine (name,lastname);
+Console.ReadLine
 
