0ee90a52375d6f23511dd5e1ebb8c77aeb980802
andrii-lazko <andrii.lazko.email@gmail.com>
2025-06-18 14:49:33 +0300

    Added_new_classes

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -1,10 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string name = "Andrii";
+string lastname = "Lazko";
 
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"Hello, World!: {name} {lastname}");
 
-Console.ReadLine
+
+Console.WriteLine();
+
+Console.ReadLine ();
+
+class Andriifunctionality
+{ 
+    public Andriifunctionality
+}
 
