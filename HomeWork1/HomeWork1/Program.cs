a9de78b472c44e72fd6cc0bf97cbcf200f65951f
andrii-lazko <andrii.lazko.email@gmail.com>
2025-06-20 15:21:51 +0300

    five and six

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+<<<<<<< Updated upstream
+=======
+Console.WriteLine("One");
+
+Console.ReadLine();
+
+Console.WriteLine("Two");
+
+Console.ReadLine();
+
+Console.WriteLine("Three");
+
+Console.WriteLine("Four");
+
+Console.WriteLine("Five");
+
+Console.WriteLine("Six");
+
+Console.ReadLine();
+
+>>>>>>> Stashed changes
 string name = "Andrii";
 string lastname = "Lazko";
 
