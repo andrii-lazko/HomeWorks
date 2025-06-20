3c3ce2911bf18b6c1311f000ec452aa3a80e6b06
andrii-lazko <andrii.lazko.email@gmail.com>
2025-06-20 17:38:25 +0300

    Lesson 3

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -1,24 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("One");
+string helloworld = "Hello World!";
+
+double val = 12.0;
+
+char c = 'A';
+
+Console.WriteLine(c);
+
+Console.WriteLine(val / 5);
 
 Console.ReadLine();
 
-Console.WriteLine("Two");
 
-Console.ReadLine();
 
-Console.WriteLine("Three");
 
-Console.WriteLine("Four");
 
-Console.ReadLine();
+//Console.WriteLine("One");
 
-string name = "Andrii";
-string lastname = "Lazko";
+//Console.ReadLine();
 
-Console.WriteLine("Hello " + name + " " + lastname);
+//Console.WriteLine("Two");
 
-Console.ReadLine();
+//Console.ReadLine();
+
+//Console.WriteLine("Three");
+
+//Console.WriteLine("Four");
+
+//Console.ReadLine();
+
+//Console.WriteLine("$$$_____$$$$$$$_$$$$$$$_$$$_______$$$_$$$$$$$$$$");
+//Console.WriteLine("$$$____$$$____$$$____$$$_$$$_____$$$__$$$_______");
+//Console.WriteLine("$$$____$$$_____$_____$$$_$$$_____$$$__$$$_______");
+//Console.WriteLine("$$$_____$$$_________$$$___$$$___$$$___$$$$$$$$__");
+//Console.WriteLine("$$$______$$$_______$$$_____$$$_$$$____$$$_______");
+//Console.WriteLine("$$$_______$$$_____$$$______$$$_$$$____$$$_______");
+//Console.WriteLine("$$$$$$$$$___$$$$$$$_________$$$$$_____$$$$$$$$$$");
+
+//Console.ReadLine();
+
+//string name = "Andrii";
+//string lastname = "Lazko";
+
+//Console.WriteLine("Hello " + name + " " + lastname);
+
+//Console.ReadLine();
 
 
