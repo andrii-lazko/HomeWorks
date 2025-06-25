7dfdbd3e446937d6fc11566fd533ba15c613d2d0
andrii-lazko <andrii.lazko.email@gmail.com>
2025-06-25 16:56:45 +0300

    homework4

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -1,21 +1,71 @@
 // See https://aka.ms/new-console-template for more information
 
+
+//дз заняття 4
+
+int inputValue = int.Parse(Console.ReadLine());
+
+bool isPrime = true;
+
+if (inputValue < 2)
+{
+    isPrime = false;
+}
+else if (inputValue == 2)
+{
+    isPrime = true;
+}
+else if (inputValue % 2 == 0)
+{
+    isPrime = false;
+}
+else 
+{
+    int divisor = 3;
+    while(divisor <= Math.Sqrt(inputValue))
+    {
+        if (inputValue % divisor == 0)
+        {
+            isPrime = false;
+            break;
+        }
+        divisor += 2;
+    }
+
+
+}
+
+if (isPrime)
+{
+    Console.WriteLine("The Prime number");
+}
+else
+{
+    Console.WriteLine("Not a Prime number");
+}
+
+
+
+
+
+
+
 //заняття 4
 
 
-int Counter = 1;
+//int Counter = 1;
 
-while (true)
-{
-    Console.Write(Counter + " ");
-    Counter++; //відняти від каунтера одиничку і записати в каунтер та сама фігня -- або ++
+//while (true) //нескінченний цикл
+//{
+//    Console.Write(Counter + " ");
+//    Counter++; //відняти від каунтера одиничку і записати в каунтер та сама фігня -- або ++
 
-if (Counter == 123)
-    {
-        break;
-    }
+//if (Counter == 123)
+//    {
+//        break;
+//    }
 
-}
+//}
 
 
 
