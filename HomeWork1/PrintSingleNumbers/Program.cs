daf1b262f46673e5e972571c9c8935a574050c1c
andrii-lazko <andrii.lazko.email@gmail.com>
2025-06-27 15:30:14 +0300

    Update Program.cs

--- HomeWork1/PrintSingleNumbers/Program.cs
+++ HomeWork1/PrintSingleNumbers/Program.cs
@@ -1,42 +1,42 @@
 ﻿
 
-int inputValue = int.Parse(Console.ReadLine());
+//int inputValue = int.Parse(Console.ReadLine());
 
-bool isPrime = true;
+//bool isPrime = true;
 
-if (inputValue < 2)
-{
-    isPrime = false;
-}
-else if (inputValue == 2)
-{
-    isPrime = true;
-}
-else if (inputValue % 2 == 0)
-{
-    isPrime = false;
-}
-else
-{
-    int divisor = 3;
-    while (divisor <= Math.Sqrt(inputValue))
-    {
-        if (inputValue % divisor == 0)
-        {
-            isPrime = false;
-            break;
-        }
-        divisor += 2;
-    }
+//if (inputValue < 2)
+//{
+//    isPrime = false;
+//}
+//else if (inputValue == 2)
+//{
+//    isPrime = true;
+//}
+//else if (inputValue % 2 == 0)
+//{
+//    isPrime = false;
+//}
+//else
+//{
+//    int divisor = 3;
+//    while (divisor <= Math.Sqrt(inputValue))
+//    {
+//        if (inputValue % divisor == 0)
+//        {
+//            isPrime = false;
+//            break;
+//        }
+//        divisor += 2;
+//    }
 
 
-}
+//}
 
-if (isPrime)
-{
-    Console.WriteLine("The Prime number");
-}
-else
-{
-    Console.WriteLine("Not a Prime number");
-}
+//if (isPrime)
+//{
+//    Console.WriteLine("The Prime number");
+//}
+//else
+//{
+//    Console.WriteLine("Not a Prime number");
+//}
